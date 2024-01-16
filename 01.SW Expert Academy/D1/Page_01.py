06030c64c0c01233929c2bef7425b0d819a59ee9
seongmin <tjdals546@gmail.com>
2024-01-16 21:35:50 +0900

    Add to Page_01.py

--- 01.SW Expert Academy/D1/Page_01.py
+++ 01.SW Expert Academy/D1/Page_01.py
@@ -8,18 +8,25 @@ sys.stdin = open('./public_input.txt', 'r')
 '''
 
 # 2072. 홀수만 더하기
-tc = int(input())
+# tc = int(input())
 
-for t in range(tc):
-  num_list = list(map(int, input().split()))
-  answer = 0
+# for t in range(tc):
+#   num_list = list(map(int, input().split()))
+#   answer = 0
 
-  for i in num_list:
-    if i%2 == 1:
-      answer += i
+#   for i in num_list:
+#     if i%2 == 1:
+#       answer += i
 
-  print('#{} {}'.format(t, answer))
+#   print('#{} {}'.format(t, answer))
 
 # ----------------------------------------------------------------------
   
-# 2072. 평균값 구하기
+# 2071. 평균값 구하기
+tc = int(input())
+
+for t in range(1, tc+1):
+    num_list = list(map(int, input().split()))
+    answer = round(sum(num_list)/10, 0)
+    
+    print('#{} {:.0f}'.format(t, answer))

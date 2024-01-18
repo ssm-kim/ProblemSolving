487fbb35cf8bb828f624d0fc07721dd614b8d41c
seongmin <tjdals546@gmail.com>
2024-01-18 23:11:15 +0900

    D1 풀이완료

--- 01.SW Expert Academy/D1/Page_01.py
+++ 01.SW Expert Academy/D1/Page_01.py
@@ -11,54 +11,114 @@ sys.stdin = open('./public_input.txt', 'r')
 '''
 
 # 2072. 홀수만 더하기
-tc = int(input())
+# tc = int(input())
 
-for t in range(tc):
-  num_list = list(map(int, input().split()))
-  answer = 0
+# for t in range(tc):
+#   num_list = list(map(int, input().split()))
+#   answer = 0
 
-  for i in num_list:
-    if i%2 == 1:
-      answer += i
+#   for i in num_list:
+#     if i%2 == 1:
+#       answer += i
 
-  print('#{} {}'.format(t, answer))
+#   print('#{} {}'.format(t, answer))
 
 # ----------------------------------------------------------------------
 
 # 2071. 평균값 구하기
-tc = int(input())
+# tc = int(input())
   
-for t in range(1, tc + 1):
-    num_list = list(map(int, input().split()))
-    answer = sum(num_list)/10
+# for t in range(1, tc + 1):
+#     num_list = list(map(int, input().split()))
+#     answer = sum(num_list)/10
     
-    print('#{} {:.0f}'.format(t, answer))
+#     print('#{} {:.0f}'.format(t, answer))
 
 # ----------------------------------------------------------------------
 
 # 2070. 큰 놈, 작은 놈, 같은 놈
-tc = int(input())
+# tc = int(input())
 
-for t in range(1, tc+1):
-    num1, num2 = map(int, input().split())
+# for t in range(1, tc+1):
+#     num1, num2 = map(int, input().split())
     
-    if num1 > num2:
-        answer = '>'
-    elif num1 < num2:
-        answer = '<'
-    else:
-        answer = '='
+#     if num1 > num2:
+#         answer = '>'
+#     elif num1 < num2:
+#         answer = '<'
+#     else:
+#         answer = '='
 
-    print('#{} {}'.format(t, answer))
+#     print('#{} {}'.format(t, answer))
 
 # ----------------------------------------------------------------------
 
 # 2063. 중간값 찾기
+# n = int(input())
+# num_list = list(map(int, input().split()))
 
+# num_list.sort()
+
+# print(num_list[n//2])
 
 # ----------------------------------------------------------------------
 
 # 2058. 자릿수 더하기
+# n = int(input())
+# remainder = 0
+
+# while n > 0:
+#     remainder += (n % 10)
+#     n = n // 10
+
+# print(remainder)
+
+# ----------------------------------------------------------------------
+
+# 2068. 최대수 구하기
+# tc = int(input())
+# for t in range(1, tc+1):
+#     num_list = list(map(int, input().split()))
+#     print('#{} {}'.format(t, max(num_list)))
+
+# ----------------------------------------------------------------------
+
+# 1545. 거꾸로 출력해 보아요
+# n = int(input())
+# for i in range(n, -1, -1):
+#     print(i, end=' ')
+
+# ----------------------------------------------------------------------
+
+# 2046. 스탬프 찍기
+# n = int(input())
+# print('#' * n)
+
+# ----------------------------------------------------------------------
+
+# 2056. 연월일 달력 
+
+# calendar = {
+#     1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
+#     7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
+# }
+
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n = input()
+#     year, month, day = n[:4], n[4:6], n[6:]
+
+#     if 1 <= int(month) <= 12 and int(day) <= calendar[int(month)]:
+#         answer = year + '/' + month + '/' + day
+#     else:
+#         answer = -1
+#     print('#{} {}'.format(t, answer))
 
 
 # ----------------------------------------------------------------------
+
+# 2047. 신문 헤드라인
+# newsPaper = input()
+# print(newsPaper.upper())
+
+

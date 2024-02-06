0846a9a8f55eff3cb3a3fad6a3e50b32245cf4d8
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-02-06 21:22:06 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,20 @@
+def solution(X, Y):
+    answer = ''
+    dictX, dictY = {i:0 for i in range(0, 10)}, {i:0 for i in range(0, 10)}
+    
+    for i in range(10):
+        dictX[i] = X.count(str(i))
+        dictY[i] = Y.count(str(i))
+    
+    for k in range(9, -1, -1):
+        minCnt = min(dictX[k], dictY[k])
+        if answer == '' and k == 0 and minCnt > 0:
+            answer = '0'
+            break
+            
+        answer += (str(k) * minCnt)
+    
+    if not answer:
+        answer = '-1'
+        
+    return answer

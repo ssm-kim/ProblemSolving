f936e04ced534a73fc81cbfb8f6a6b7cd46141b7
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-02-11 17:59:29 +0900

    Update 햄버거 만들기.py

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -4,11 +4,14 @@ def solution(ingredient):
     for i in range(len(ingredient)):
         stack.append(ingredient[i])
         if stack[-4:] == [1, 2, 3, 1]:
-            # stack = stack[:len(stack)-4]
-            stack.pop()
-            stack.pop()
-            stack.pop()
-            stack.pop()
+            # stack = stack[:len(stack)-4]  >  시간초과
+            # 방법 1
+            del stack[-4:]
+            # 방법 2
+            # stack.pop()
+            # stack.pop()
+            # stack.pop()
+            # stack.pop()
             answer += 1
             
     return answer

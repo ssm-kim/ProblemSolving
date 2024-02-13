e10d36757310cccbcc3ed09fa46e9d67a15fb267
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-02-14 02:16:33 +0900

    Update 달리기 경주.py

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -2,19 +2,21 @@ def solution(players, callings):
     answer = []
     rank = { players[i]:i for i in range(len(players)) }
     pos = players  # 원래 선수들 위치 리스트
-    
+    # print(rank, pos)
+    # pos 리스트로 선수 위치 확인, rank 딕셔너리로 등수 확인
     for call in callings:
         tmp = pos[rank[call]]
-        frontIdx = pos[rank[call]]   # 따라잡힌 선수 인덱스번호
+        frontIdx = pos[rank[call]]   # 따라잡힌 선수이름
         
         pos[rank[call]] = pos[rank[call]-1]
-        backIdx = pos[rank[call]-1]  # 따라잡는 선수 인덱스번호
+        backIdx = pos[rank[call]-1]  # 따라잡는 선수이름
+        
         pos[rank[call]-1] = tmp
         
         # print(pos, frontIdx, backIdx)
         rank[frontIdx] -= 1
         rank[backIdx] += 1
-        
+    # print(rank, pos)
     answer = pos
     
     return answer

0f0d2bdf75dd9ac49c5fa02b8d879bdf9deba3de
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-02-15 01:48:12 +0900

    Update 개인정보 수집 유효기간.py

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,47 +1,35 @@
-# 2000 <= YYYY <= 2022, 1 <= MM <= 12, 1 <= DD <= 28
 def expiredCheck(today, period, date):  # 오늘날짜, 약관에 따른 유효기간, 수집일자
-    # date(수집일자)에 period(유효기간)를 더한 값 출력
-    # date만 날짜를 추가하고 다시 yyyy.mm.dd 형태로 변경 후 today와 비교
-    addDays = int(period) * 28
+    # 연월일범위 : 2000 <= YYYY <= 2022, 1 <= MM <= 12, 1 <= DD <= 28
+    addDays = int(period) * 28  # date(수집일자)에 period(유효기간)를 더한 값 출력
     year, month, day = map(int, date.split('.'))
-    print(today, period, date)
     
-    # 일자 계산
-    day += (addDays-1)
+    day += (addDays-1)  # 일자 계산
     addMonth = day//28
     day %= 28
     if day == 0:  # 일자가 0일 때
         day = 28
         addMonth -= 1
-    print(year, month, day)
-    # 월 계산
-    month += addMonth
-    print(year, month, day)
+    
+    month += addMonth  # 월 계산
     if month > 12:
         addYear = month//12
         month %= 12
-    elif month == 0:  # 월이 0일 때
-        month = 12
-        addYear = -1
     else:
         addYear = 0
-    
-    # 연 계산
-    if addYear:
-        year += addYear
-    if month == 0:
+    if month == 0:   # 월 계산이후 0월달이라면
         month = 12
         year -= 1
-    print(year, month, day)
+        
+    if addYear:  # 연 계산
+        year += addYear
     
     if day < 10:
         day = '0' + str(day)
     if month < 10:
         month = '0' + str(month)
     
-    date = str(year) + '.' + str(month) + '.' + str(day)
-    print(today, date)
-    print()
+    date = str(year) + '.' + str(month) + '.' + str(day)  # yyyy.mm.dd 형태로 변경 후 today와 비교
+
     if today > date:
         return True
     else:
@@ -54,8 +42,7 @@ def solution(today, terms, privacies):
         k, v = x.split()
         terms_dict[k] = v
     
-    # privacies 유효기간이 지나지 않은 약관의 번호를 answer에 추가
-    for i in range(len(privacies)):
+    for i in range(len(privacies)):  # privacies 유효기간이 지나지 않은 약관의 번호를 answer에 추가
         date, condition = privacies[i].split()
         if expiredCheck(today, terms_dict[condition], date):   # 오늘날짜, 약관에 따른 유효기간, 수집일자
             answer.append(i+1)

d2c9f86f272226dcc7962d20b59cb2c1ffb97966
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-02-16 00:37:29 +0900

    Update 신고 결과 받기.py

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,25 +1,26 @@
 def solution(id_list, report, k):
-    answer = []
-    report = list(set(report))
-    receivedReport = { id_list[i]:0 for i in range(len(id_list)) }
-    userId_to_reportId = { id_list[i]:list() for i in range(len(id_list)) }
+    answer = { id_list[i]:0 for i in range(len(id_list)) }
+    report = list(set(report))  # 중복 제거
+    receivedReport = { id_list[i]:0 for i in range(len(id_list)) }  # 유저별 신고당한 횟수
+    userId_to_reportId = { id_list[i]:list() for i in range(len(id_list)) }  # 유저ID와 유저가 신고한 ID
     
     for x in report:
         request, response = x.split()
         receivedReport[response] += 1
         userId_to_reportId[request].append(response)
     
-    
-    stopId = list()
+    stopId = list()  # 게시판 이용정지된 ID
     for key, value in receivedReport.items():
-        if value >= k :
+        if value >= k :  # k번 이상 신고당한 유저라면 추가
             stopId.append(key)
     
+    # 유저ID와 유저가 신고한 ID 중에서 이용정지된 ID가 있다면 +1
     for key, value in userId_to_reportId.items():
         cnt = 0
         for id in stopId:
             if id in value:
-                cnt += 1
-        answer.append(cnt)    
+                answer[key] += 1
+    
+    answer = list(answer.values())  # 유저ID와 유저가 신고한 ID의 이용정지된 ID 총 갯수
     
     return answer

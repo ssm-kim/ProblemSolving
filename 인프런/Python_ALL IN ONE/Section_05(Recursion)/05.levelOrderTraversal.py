22edcdacbafe5004554ad2718e90058422e602c4
seongmin <tjdals546@gmail.com>
2024-03-24 23:50:39 +0900

    SWEA D3 1 Page 완료

--- 인프런/Python_ALL IN ONE/Section_05(Recursion)/05.levelOrderTraversal.py
+++ 인프런/Python_ALL IN ONE/Section_05(Recursion)/05.levelOrderTraversal.py
@@ -35,3 +35,25 @@
 
 # BFS 풀이 먼저 시작
 
+from collections import deque
+
+def maxDepth(root):
+    max_depth = 0
+    if root is None:
+        return max_depth
+    
+    q = deque()
+    q.append((root, 1))  # 처음 시작할 때 깊이는 1
+
+    while q:
+        cur_node, cur_depth = q.popleft()  # 현재 노드와 깊이반환
+        max_depth = max(max_depth, cur_depth)
+        if cur_node.left:
+            q.append((cur_node.left, cur_depth + 1))
+        if cur_node.right:
+            q.append((cur_node.right, cur_depth + 1))
+    return max_depth
+
+root = [3, 9, 20, None, None, 15, 7]
+
+maxDepth(root)

4926b1176ec33a3e19400dcaae53ddfb2cd7a907
seongmin <tjdals546@gmail.com>
2024-03-27 00:06:05 +0900

    1220 풀이 예정

--- SW Expert Academy/D3/Page_02.py
+++ SW Expert Academy/D3/Page_02.py
@@ -54,9 +54,56 @@ sys.stdin = open('./public_input.txt', 'r')
 
 # 1215. [S/W 문제해결 기본] 3일차 - 회문1
 '''
-     문제 이해 및 접근방법
+    문제 이해 및 접근방법
+     - 똑바로 읽어도 거꾸로 읽어도 똑같은 문장이나 낱말을 회문(回文, palindrome)
+     - 8x8 평면 글자판에서 제시된 길이를 가진 회문의 개수
+     - 가로 또는 세로로 이어진 회문의 개수만 센다.
 '''
+# def palindrome(check):
+#     for i in range(len(check)//2):
+#         if check[i] != check[-1-i]:
+#             return False
+#     return True
 
-
-
+# tc = 10
+# for t in range(1, tc+1):
+#     n = int(input())
+#     board = [list(input()) for _ in range(8)]
+#     cnt = 0
+#     for i in range(len(board)):
+#         col = 0
+#         for j in range(8-n+1):
+#             if palindrome(board[i][j:j+n]):  # 가로 n길이 문자수 체크
+#                 cnt += 1
+#             s = ''
+#             for k in range(n):
+#                 s += board[k+col][i]  
+#             if palindrome(s):  # 세로 n길이 문자수 체크
+#                 cnt += 1
+#             col += 1
+#     print('#{} {}'.format(t, cnt))
+    
 # ----------------------------------------------------------------------
+
+# 5601. [Professional] 쥬스 나누기
+'''
+    문제 이해 및 접근방법
+'''
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n = int(input())
+#     print('#{}'.format(t), end=' ')
+#     for i in range(1, n+1):
+#         print('{}/{} '.format(1, n), end='')
+#     print()
+    
+# ----------------------------------------------------------------------
+
+# 1220.[S/W 문제해결 기본] 5일차 - Magnetic
+'''
+    문제 이해 및 접근방법
+'''
+tc = int(input())
+for t in range(1, tc+1):
+    n = int(input())
+    break

715677e201f86b8be24ae651e6bce0a2bc17f96b
seongmin <tjdals546@gmail.com>
2024-04-01 01:42:30 +0900

    4페이지 풀이 3페이지 회문2 다시 풀이

--- SW Expert Academy/D3/Page_03.py
+++ SW Expert Academy/D3/Page_03.py
@@ -153,21 +153,123 @@ sys.stdin = open('./public_input.txt', 'r', encoding='utf-8')
 
 # ----------------------------------------------------------------------
 
-# 1240. [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드  >  Pass
+# 1240. [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드
 '''
      문제 이해 및 접근방법
+          - 암호코드는 8개의 숫자로 이루어져 있다.
+          - 올바른 암호코드는 (홀수 자리의 합 x 3) + (짝수 자리의 합)이 10의 배수가 되어야 한다.
+          - 길이가 56가 아닌 코드는 주어지지 않는다.
 '''
-tc = int(input())
-for t in range(1, tc+1):
-    n, m = map(int, input().split())
-    board = [list(input()) for _ in range(n)]
-    for row in board:
-        if '1' in row:
-            print(''.join(row))
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n, m = map(int, input().split())
+#     psCode = {'0001101':0, '0011001':1, '0010011':2, '0111101':3, '0100011':4,
+#               '0110001':5, '0101111':6, '0111011':7, '0110111':8, '0001011':9}
+#     board = [list(input()) for _ in range(n)]
+#     answer = 0
+#     for rows in board:
+#         tmp = ''
+#         if '1' in rows:
+#             lt = ''.join(rows).index('1')
+#             for i in range(lt, len(rows)):
+#                 if '1' not in rows[i:]:
+#                     break
+#                 tmp += rows[i]
+#             checkLen = 56-len(tmp)   # 56길이보다 작으면
+#             if checkLen > 0: 
+#                 tmp = ('0' * checkLen) + tmp    
+
+#             bit = list()
+#             for i in range(0, len(tmp), 7):  # 7자로 분할
+#                 bit.append(psCode[tmp[i:i+7]])
+            
+#             # 검증
+#             even, odd = 0, 0
+#             for i in range(len(bit)):
+#                 if (i%2) == 0:
+#                     even += bit[i]
+#                 else:
+#                     odd += bit[i]
+#             total = even*3 + odd
+    
+#             if total%10 == 0:
+#                 answer = sum(bit)
+#     print('#{} {}'.format(t, answer))
+     
     
 # ----------------------------------------------------------------------
 
-# 1240. [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드  >  Pass
+# 10804. 문자열의 거울상
 '''
      문제 이해 및 접근방법
 '''
+# tc = int(input())
+# for t in range(1, tc+1):
+#     s = input()
+#     answer = ''
+#     for i in s:
+#         if i == 'b':
+#             answer = 'd' + answer
+#         elif i == 'd':
+#             answer = 'b' + answer
+#         elif i == 'p':
+#             answer = 'q' + answer
+#         else:
+#             answer = 'p' + answer
+#     print('#{} {}'.format(t, answer))
+
+# ----------------------------------------------------------------------
+
+# 1216. [S/W 문제해결 기본] 3일차 - 회문2  >  다시 풀어보기
+'''
+    문제 이해 및 접근방법
+        - "기러기" 또는 "level" 과 같이 거꾸로 읽어도 제대로 읽은 것과 같은 문장이나 낱말을 회문(palindrome)
+        - 주어진 100x100 평면 글자판에서 가로, 세로를 모두 보아 가장 긴 회문의 길이를 구해라
+'''
+def check(s):  # 회문판별함수 완료
+    half = len(s)//2
+    if (len(s)%2) == 0:
+        return s[:half] == ''.join(reversed(s[half:]))
+    else:
+        return s[:half] == ''.join(reversed(s[half+1:]))
+
+tc = 10
+for t in range(1, tc+1):
+    t = int(input())
+    row = [list(input()) for _ in range(100)]
+    col = list()
+    for i in range(100):
+        s = ''
+        for j in range(100):
+            s += row[j][i]
+        col.append(s)
+
+    # 0행: 0 ~ 99, 1 ~ 99, 2 ~ 99, 3 ~ 99 ... 
+    # 1행: 0 ~ 99, 1 ~ 99, 2 ~ 99, 3 ~ 99 ... 
+    # 99행까지 반복
+    answer = 0
+    for i in range(len(row)):  
+        for j in range(len(row)+1):
+            for k in range(len(row)):
+                if check(''.join(row[k][i:j])) or check(''.join(col[k][i:j])):
+                    answer = max(answer, len(row[k][i:j]), len(row[k][i:j]))
+    print('#{} {}'.format(t, answer))
+
+# ----------------------------------------------------------------------
+
+# 14692. 통나무 자르기
+'''
+    문제 이해 및 접근방법
+        1. 게임은 Alice가 먼저 시작하며 그 이후 둘이 번갈아가면서 턴을 가진다.
+        2. 각 턴을 맡은 사람은 통나무를 두 조각으로 나누는데,
+           이 때 잘린 통나무가 모두 자연수(1 이상의 정수) 미터 길이를 가지도록 잘라야 한다.
+        3. 더 이상 자를 수 없게 되는 사람이 진다. 누가 이기는가?
+'''
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n = int(input())
+#     if n%2 == 0:
+#         answer = 'Alice'
+#     else:
+#         answer = 'Bob'
+#     print('#{} {}'.format(t, answer))

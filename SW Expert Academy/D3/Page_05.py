b3d8a991b7364d934233c351f8a18d7bc16a4ce4
seongmin <tjdals546@gmail.com>
2024-04-04 00:27:54 +0900

    6페이지 시작

--- SW Expert Academy/D3/Page_05.py
+++ SW Expert Academy/D3/Page_05.py
@@ -148,8 +148,91 @@ sys.stdin = open('./public_input.txt', 'r')
 # 13547. 팔씨름
 '''
     문제 이해 및 접근방법
+        - 둘은 15번 팔씨름을 하여 8번 이상 이기는 사람이 점심 값을 면제받기로 하였다.
+        - S[i]가 ‘o’면 소정이가 i번째 경기에서 승리했다는 것이고, ‘x’면 패배했다는 것이다.
+    
+    접근방법
+        - 문제의도는 면제받을가능성 유무를 파악하는 것
+        - 순서가 중요하지 않으므로 총 15글자에서 절반이상이 'o'이거나 절반미만이 'x'라면
+          면제받을가능성이 존재한다.
 '''
-
-
+# tc = int(input())
+# for t in range(1, tc+1):
+#     s = input()
+#     answer = 'NO'
+#     if s.count('o') >= 8 or s.count('x') <= 7:
+#         answer = 'YES'
+#     print('#{} {}'.format(t, answer))
 
 # ----------------------------------------------------------------------
+    
+# 11736. 평범한 숫자
+'''
+    문제 이해 및 접근방법
+        - 1 이상 N 이하의 정수가 적혀 있는 길이 N의 순열 p1, p2, …, pN이 있다.
+        - 수열에 있는 모든 숫자는 서로 다르다.
+        - (핵심) 2 ≤ i ≤ N-1이며
+                 pi-1, pi, pi+1 중 pi가 최솟값도, 최댓값도 아니라면 pi를 평범한 숫자라고 정의한다.
+        -  전체 배열에서의 최솟값과 최댓값이 아니라 양옆 숫자랑 비교하는 것.
+'''
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n = int(input())
+#     p = list(map(int, input().split()))
+#     basicNum = 0
+#     for i in range(1, n-1):
+#         low, high = min(p[i-1], p[i], p[i+1]), max(p[i-1], p[i], p[i+1])
+#         if low < p[i] < high:
+#             basicNum += 1
+#     print('#{} {}'.format(t, basicNum))
+
+# ----------------------------------------------------------------------
+    
+# 1221. [S/W 문제해결 기본] 5일차 - GNS
+'''
+    문제 이해 및 접근방법
+        - 0 ~ 9 의 값을 나타내는 단어가 섞여 있는 문자열을 받아 작은 수부터 차례로 정렬하여 출력
+    접근방법
+        1. 딕셔너리 생성 ("ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN")
+        2. 문자열 입력받을 시 미리생성한 딕셔너리를 활용하여 숫자로 변환하여 리스트에 담는다.
+        3. 숫자로 구성된 리스트 정렬
+        4. 미리 생성된 딕셔너리의 키, 값을 서로 변경
+        5. 정렬된 리스트를 변경된 딕셔너리를 활용하여 값 출력
+'''
+# tc = int(input())
+# for _ in range(1, tc+1):
+#     t, n = input().split()
+#     n = int(n)
+#     planet = { "ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4,
+#                "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9 }
+#     nums = [planet[i] for i in list(input().split())]  # 입력받은 리스트를 바로 숫자로 변환
+#     nums.sort()  # 정렬
+
+#     planet = { v:k for k, v in planet.items() }  # planet 딕셔너리의 key, value값 변경
+#     words = [planet[i] for i in nums]
+#     print('{}\n{}'.format(t, ' '.join(words)))
+
+# ----------------------------------------------------------------------
+
+# 1206. [S/W 문제해결 기본] 1일차 - View
+'''
+    문제 이해 및 접근방법
+        - 왼쪽과 오른쪽으로 창문을 열었을 때, 양쪽 모두 거리 2 이상의 공간이 확보될 때 조망권이 확보
+        - 조망권이 확보된 세대의 수를 반환하여라.
+        - 맨 왼쪽 두 칸과 맨 오른쪽 두 칸에 있는 건물은 항상 높이가 0이다.
+'''
+# tc = 10
+# for t in range(1, tc+1):
+#     n = int(input())
+#     height = list(map(int, input().split()))
+#     secureCnt = 0
+#     for i in range(2, n-2):
+#         buildings = height[i-2:i+3]
+#         mid = buildings.pop(2)
+#         if mid > max(buildings):
+#             secureCnt += (mid - max(buildings))
+#     print('#{} {}'.format(t, secureCnt))
+
+# ----------------------------------------------------------------------
+
+# 1229. [S/W 문제해결 기본] 8일차 - 암호문2  >  암호문3번 문제를 풀었으므로 Pass

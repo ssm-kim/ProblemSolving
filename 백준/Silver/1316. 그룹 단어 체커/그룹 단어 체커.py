e0355ce36c4c7f5e511f2c5477f90b175c4b8755
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-05-06 23:47:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,10 +2,10 @@ def check(word):
     for i in word:
         if i not in st:
             st.append(i)
-            curr = i     # 현재문자
-        elif curr == i:  # 다음문자와 현재문자가 같다면 반복문 유지
+            curr = i
+        elif curr == i:
             continue
-        else:            # 현재문자가 st에 포함되고 다음문자와 다르다면 False 리턴
+        else:
             return False
     return True
 
@@ -13,7 +13,7 @@ tc = int(input())
 cnt = 0
 for t in range(1, tc+1):
     word = input()
-    st = list()  # 중복문자확인
-    if check(word):  # 그룹 단어라면 True 반환
+    st = list()
+    if check(word):
         cnt += 1
 print(cnt)

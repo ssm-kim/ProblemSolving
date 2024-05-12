373d519ec0658a3ef05a5bfe136d3cf77ffc4384
seongmin <92963604+ssm-kim@users.noreply.github.com>
2024-05-12 14:13:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,11 @@
 n = int(input())
-start = 0
-checkNum = 0
+start = 665  # 첫 시작 666-1
+cnt = 0      # 종말의 숫자라면 카운트
+
 while True:
-    if n == checkNum:
+    if n == cnt:
         break
     start += 1
-    if "666" in str(start):
-        checkNum += 1
-
+    if '666' in str(start):
+        cnt += 1
 print(start)

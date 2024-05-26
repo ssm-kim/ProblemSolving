9bbe32ff15a884e5c19136d616ae8da2080f1f32
seongmin <tjdals546@gmail.com>
2024-05-26 20:14:09 +0900

    기하: 직사각형과 삼각형 4번문제 풀이 예정

--- BaeJoon/01.단계별로 풀어보기/10.기하 직사각형과 삼각형/1-1_직사각형과삼각형.py
+++ BaeJoon/01.단계별로 풀어보기/10.기하 직사각형과 삼각형/1-1_직사각형과삼각형.py
@@ -9,6 +9,39 @@ sys.stdin = open('../input.txt', 'r')
 '''
     문제풀이방법 및 접근
 '''
+# a = int(input())
+# b = int(input())
 
+# print(a * b)
+
+# --------------------------------------------------
+
+# 1085. 직사각형에서 탈출
+'''
+    문제풀이방법 및 접근
+'''
+# x, y, w, h = map(int, input().split())
+
+# print(min(w-x, h-y, x, y))
+
+# --------------------------------------------------
+
+# 3009. 네번째 점
+'''
+    문제풀이방법 및 접근
+'''
+x = []
+y = []
+for _ in range(3):
+    a, b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+
+for i in range(3):
+    if x.count(x[i]) == 1:
+        x1 = x[i]
+    if y.count(y[i]) == 1:
+        y1 = y[i]
+print(x1, y1)
 
 # --------------------------------------------------

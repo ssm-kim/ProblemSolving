b0299b27b721a91c379a2f36577bea3c16d74e4b
seongmin <tjdals546@gmail.com>
2024-05-27 22:47:48 +0900

    기하:직사각형과삼각형 마지막문제예정

--- BaeJoon/01.단계별로 풀어보기/10.기하 직사각형과 삼각형/1-1_직사각형과삼각형.py
+++ BaeJoon/01.단계별로 풀어보기/10.기하 직사각형과 삼각형/1-1_직사각형과삼각형.py
@@ -30,18 +30,105 @@ sys.stdin = open('../input.txt', 'r')
 '''
     문제풀이방법 및 접근
 '''
-x = []
-y = []
-for _ in range(3):
-    a, b = map(int, input().split())
-    x.append(a)
-    y.append(b)
+# x = []
+# y = []
+# for _ in range(3):
+#     a, b = map(int, input().split())
+#     x.append(a)
+#     y.append(b)
 
-for i in range(3):
-    if x.count(x[i]) == 1:
-        x1 = x[i]
-    if y.count(y[i]) == 1:
-        y1 = y[i]
-print(x1, y1)
+# for i in range(3):
+#     if x.count(x[i]) == 1:
+#         x1 = x[i]
+#     if y.count(y[i]) == 1:
+#         y1 = y[i]
+# print(x1, y1)
 
 # --------------------------------------------------
+
+# 15894. 수학은 체육과목 입니다
+'''
+    문제풀이방법 및 접근
+'''
+# n = int(input())
+# print(n*4)
+
+# --------------------------------------------------
+
+# 9063. 대지
+'''
+    문제풀이방법 및 접근
+        - (x의 가장큰값 - x의 가장작은값) * (y의 가장큰값 - y의 가장작은값)
+        - 초기값 세팅을 0, 10001로 하면 안됨.
+'''
+# n = int(input())
+# coordinate = [list(map(int, input().split())) for _ in range(n)]
+
+# if n == 1:
+#     answer = 0
+# else:
+#     row, col = list(), list()
+#     for x, y in coordinate:
+#         row.append(x)
+#         col.append(y)
+    
+#     answer = (max(row)-min(row)) * (max(col)-min(col))
+# print(answer)
+
+# --------------------------------------------------
+
+# 10101. 삼각형 외우기
+'''
+    문제풀이방법 및 접근
+        - 세 각의 크기가 모두 60이면, Equilateral
+        - 세 각의 합이 180이고, 두 각이 같은 경우에는 Isosceles
+        - 세 각의 합이 180이고, 같은 각이 없는 경우에는 Scalene
+        - 세 각의 합이 180이 아닌 경우에는 Error
+'''
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# answer = ''
+# if a == 60 and b == 60 and c == 60:
+#     answer = 'Equilateral'
+# elif (a+b+c == 180) and (a == b or b == c or a == c):
+#     answer = 'Isosceles'
+# elif (a+b+c == 180) and a != b and b != c:
+#     answer = 'Scalene'
+# else:
+#     answer = 'Error'
+# print(answer)
+
+# --------------------------------------------------
+
+# 5073. 삼각형과 세 변
+'''
+    문제풀이방법 및 접근
+'''
+# while True:
+#     a, b, c = map(int, input().split())
+#     if a==0 and b==0 and c==0:
+#         break
+
+#     answer = ''
+#     tmp = [a, b, c]
+#     longest = max(tmp)
+#     tmp.remove(longest)
+
+#     if longest >= sum(tmp):
+#         answer = 'Invalid'
+#     elif a==b and b==c:
+#         answer = 'Equilateral'
+#     elif a==b or b==c or a==c:
+#         answer = 'Isosceles'
+#     elif a!=b and b!=c and a!=c:
+#         answer = 'Scalene'
+#     print(answer)
+
+# --------------------------------------------------
+
+# 14215. 세 막대
+'''
+    문제풀이방법 및 접근
+'''
+a, b, c = map(int, input().split())

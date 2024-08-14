2bd20e6911409ce7f4c6d4b92132ec2120646e1c
김성민 <rlacjs546@naver.com>
2024-08-15 00:26:50 +0900

    SWEA_2001_파리퇴치

--- SW Expert Academy/D2/Java/SWEA_2001.java
+++ SW Expert Academy/D2/Java/SWEA_2001.java
@@ -1,44 +1,43 @@
-package D2;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class SWEA_2001 {
-    public static void main(String[] args) throws FileNotFoundException {
+public class main {
 
-        System.setIn(new FileInputStream("SWEA/src/input.txt"));
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./input.txt"));
+		Scanner sc = new Scanner(System.in);
 
-        for (int tc = 1; tc < T + 1; tc++) {
-            int n = sc.nextInt(), m = sc.nextInt();
-            int [][] board = new int[n][n];
-            int max = 0;  // 파리 죽인 최댓값
+		int T = sc.nextInt();
 
-            for (int i = 0; i < n; i++) {  // 입력값 받기
-                for (int j = 0; j < n; j++) {
-                    board[i][j] = sc.nextInt();
-                }
-            }
+		for (int tc = 0; tc < T; tc++) {
+			int n = sc.nextInt(); // 표의 크기
+			int m = sc.nextInt(); // 합을 구해야 하는 횟수
 
-            for (int i = 0; i < n - m + 1; i++) {
-                for (int j = 0; j < n - m + 1; j++) {
+			int[][] arr = new int[n][n];
 
-                    int cnt = 0;  // m * m 범위에서 파리를 잡은 총 갯수
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
 
-                    for (int k = i; k < i+m; k++) {
-                        for (int l = j; l < j+m; l++) {
-                            cnt += board[k][l];
-                        }
-                    }
-
-                    if (cnt > max) {
-                        max = cnt;
-                    }
-                }
-            }
-            System.out.printf("#%d %d\n", tc, max);
-        }
-    }
+			int ans = Integer.MIN_VALUE;
+			// m * m 파리채 영역만큼 처리할 수 있으니 row, col에 각각 (- m + 1)만큼 해준다.
+			for (int i = 0; i < n - m + 1; i++) {
+				for (int j = 0; j < n - m + 1; j++) {
+					int sum = 0;
+					// m * m 파리채 영역 시작  i, j 기준 
+					for (int k = i; k < m + i; k++) {
+						for (int l = j; l < m + j; l++) {
+							sum += arr[k][l];
+						}
+					}
+					ans = Math.max(ans, sum);
+				}
+			}
+			System.out.println("#" + (tc+1) + " " + ans);
+		}
+	}
 }

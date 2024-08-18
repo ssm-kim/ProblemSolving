62692af551de20288122addab9d30f9cfb247b96
김성민 <rlacjs546@naver.com>
2024-08-18 15:26:03 +0900

    BOJ_2023 신기한 소수 (dfs)

--- BaekJoon/Java/알고리즘분류/DFS/BOJ_2023.java
+++ BaekJoon/Java/알고리즘분류/DFS/BOJ_2023.java
@@ -6,49 +6,56 @@ import java.util.*;
 
 public class Main {
 
-    public static int N;
-    public static StringBuilder sb = new StringBuilder();
+    public static int N;  // 자릿수
+    public static StringBuilder sb = new StringBuilder();  // 결과 저장
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
+        // dfs (깊이 0, 시작 숫자 0)
         dfs(0, 0);
 
-        System.out.print(sb);
-        br.close();
+        System.out.println(sb);
     }
 
-    public static void dfs(int depth, int num) {
+    // depth (노드 겸 현재 자릿수), digit (현재 숫자)
+    static void dfs(int depth, int curNum) {
+        // 목표 자릿수 도달 시 결과에 추가
         if (depth == N) {
-            sb.append(num).append("\n");
+            sb.append(curNum).append("\n");
             return;
         }
 
-        for (int i = 1; i <= 9; i++) {
-            int next = num * 10 + i;
-            if (isPrime(next)) {
-                dfs(depth + 1, next);
+        // 1부터 9까지의 숫자를 현재 숫자에 추가해서 판별
+        for (int i = 1; i < 10; i++) {
+
+            // 다음 숫자 계산 (현재 숫자 * 10 + i)
+            int nextNum = 10 * curNum + i;
+
+            // 다음 숫자가 소수일 경우, 재귀 호출
+            if (isPrime(nextNum)) {
+                dfs(depth + 1, nextNum);
             }
         }
     }
 
-    static boolean isPrime(int n) {
-        // 2보다 작은 수는 소수가 아니다.
-        if (n < 2) {
+    // 소수 판별 함수
+    static boolean isPrime(int num) {
+        // 0과 1은 소수가 아님
+        if (num == 0 || num == 1) {
             return false;
         }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            // n이 i로 나누어 떨어지면 n은 소수가 아니다.
-            if (n % i == 0) {
+        // 2부터 sqrt(num)까지 나누어 떨어지는 수가 있는지 확인
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
 
-        // 위의 조건에 해당하지 않으면 n은 소수이다.
+        // 나누어 떨어지는 수가 없으면 소수
         return true;
     }
-
 }

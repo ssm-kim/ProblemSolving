857cb7e21a404788a77053e79201051e4dbafcf1
김성민 <rlacjs546@naver.com>
2024-09-26 23:35:03 +0900

    BOJ_1987_알파벳(dfs)

--- BaekJoon/Java/알고리즘분류/Graph_dfs/BOJ_1987.java
+++ BaekJoon/Java/알고리즘분류/Graph_dfs/BOJ_1987.java
@@ -12,57 +12,108 @@ import java.util.StringTokenizer;
  * 2. 해시 테이블 기반이므로 속도가 빠르다. 평균적으로 O(1)
  * 3. 메모리 사용량이 많다.
  */
+
 public class Main {
-    static int R, C, maxLen;
-    static char[][] board;
+
+    static int N, M, answer = 0;
+    static char[][] map;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-    static HashSet<Character> alphaSet = new HashSet<>();
+    static boolean[] visited = new boolean[26];  // 알파벳 26개에 대한 방문 여부
+    // static ArrayList<Character> arr;
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
-
-        for (int i = 0; i < R; i++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        for (int i = 0; i < N; i++) {
             char[] tmp = br.readLine().toCharArray();
-            for (int j = 0; j < tmp.length; j++) {
-                board[i][j] = tmp[j];
+            for (int j = 0; j < M; j++) {
+                map[i][j] = tmp[j];
             }
         }
 
-        dfs(1, 0, 0);
-        System.out.println(maxLen);
+        visited[map[0][0] - 'A'] = true;
+        dfs(0, 0, 1);
+        System.out.println(answer);
     }
 
-    static void dfs(int depth, int cx, int cy) {
-        // 기저 조건 : 현재 알파벳이 이미 사용된 경우
-        if (alphaSet.contains(board[cx][cy])) {
-            return;
-        }
+    static void dfs(int cx, int cy, int count) {
 
-        // 현재 알파벳 추가
-        alphaSet.add(board[cx][cy]);
-        maxLen = Math.max(maxLen, depth);
+        answer = Math.max(answer, count);
 
         for (int i = 0; i < 4; i++) {
             int nx = cx + dx[i];
             int ny = cy + dy[i];
 
-            // 범위 검사
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
                 continue;
             }
-            dfs(depth + 1, nx, ny);
+
+            int idx = map[nx][ny] - 'A';
+            if (!visited[idx]) {
+                visited[idx] = true;
+                dfs(nx, ny, count + 1);
+                visited[idx] = false;
+            }
         }
-        // 현재 알파벳 제거 (백트래킹)
-        alphaSet.remove(board[cx][cy]);
     }
 }
+
+//public class Main {
+//    static int R, C, maxLen;
+//    static char[][] board;
+//    static int[] dx = {0, 0, 1, -1};
+//    static int[] dy = {1, -1, 0, 0};
+//    static HashSet<Character> alphaSet = new HashSet<>();
+//
+//    public static void main(String[] args) throws IOException {
+//        System.setIn(new FileInputStream("input.txt"));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        R = Integer.parseInt(st.nextToken());
+//        C = Integer.parseInt(st.nextToken());
+//        board = new char[R][C];
+//
+//        for (int i = 0; i < R; i++) {
+//            char[] tmp = br.readLine().toCharArray();
+//            for (int j = 0; j < tmp.length; j++) {
+//                board[i][j] = tmp[j];
+//            }
+//        }
+//
+//        dfs(1, 0, 0);
+//        System.out.println(maxLen);
+//    }
+//
+//    static void dfs(int depth, int cx, int cy) {
+//        // 기저 조건 : 현재 알파벳이 이미 사용된 경우
+//        if (alphaSet.contains(board[cx][cy])) {
+//            return;
+//        }
+//
+//        // 현재 알파벳 추가
+//        alphaSet.add(board[cx][cy]);
+//        maxLen = Math.max(maxLen, depth);
+//
+//        for (int i = 0; i < 4; i++) {
+//            int nx = cx + dx[i];
+//            int ny = cy + dy[i];
+//
+//            // 범위 검사
+//            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+//                continue;
+//            }
+//            dfs(depth + 1, nx, ny);
+//        }
+//        // 현재 알파벳 제거 (백트래킹)
+//        alphaSet.remove(board[cx][cy]);
+//    }
+//}
 
 /**
  * 아래는 ArrayList와 visited 2차원 배열을 통한 구현

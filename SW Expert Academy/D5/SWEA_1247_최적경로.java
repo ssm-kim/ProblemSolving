6e9a56c6f7c37c1eef222a1c77e9753c5f31bc8f
김성민 <rlacjs546@naver.com>
2024-10-11 02:28:38 +0900

    SWEA_1247_최적경로

--- SW Expert Academy/D5/SWEA_1247_최적경로.java
+++ SW Expert Academy/D5/SWEA_1247_최적경로.java
@@ -1,76 +1,71 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Solution {
 
-	static int n, m, answer;
-	static int[] company, home, customer;
-	static int[][] map;
+	static int N, answer;
+	static int[] home, company, arr;
+	static int[][] customer;
 	static boolean[] visited;
 
-	public static void main(String[] args) throws FileNotFoundException {
-
-		System.setIn(new FileInputStream("./src/input.txt"));
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			n = sc.nextInt();
-			company = new int[] { sc.nextInt(), sc.nextInt() }; // 회사 좌표
-			home = new int[] { sc.nextInt(), sc.nextInt() }; // 집 좌표
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			home = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+			company = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
 
-			map = new int[n][2]; // 고객들의 좌표
-			for (int i = 0; i < n; i++) {
-				map[i][0] = sc.nextInt();
-				map[i][1] = sc.nextInt();
+			customer = new int[N][2];
+			for (int i = 0; i < N; i++) {
+				customer[i][0] = Integer.parseInt(st.nextToken());
+				customer[i][1] = Integer.parseInt(st.nextToken());
 			}
 
-			// 순열
+			visited = new boolean[N];
+			arr = new int[N];
 			answer = Integer.MAX_VALUE;
-			visited = new boolean[n];
-			customer = new int[n];
-			dfs(0);
+			permutation(0);
+
 			System.out.println("#" + tc + " " + answer);
 		}
 	}
 
-	static void dfs(int depth) {
-		if (depth == n) {
-			int distance = 0; // 누적 거리
+	private static void permutation(int depth) {
+		if (depth == N) {
+			int distance = 0;
+			// System.out.println("순열 : " + Arrays.toString(arr));
+			// 회사에서 첫번째 고객집으로 출발
+			int[] start = customer[arr[0]];
 
-			// 회사에서 첫번째 고객 목적지까지 계산
-			int x1 = company[0], y1 = company[1];
-			int x2 = map[customer[0]][0], y2 = map[customer[0]][1];
-			distance += Math.abs(x1 - x2) + Math.abs(y1 - y2);
+			distance += Math.abs(Math.abs(company[0] - start[0]) + Math.abs(company[1] - start[1]));
 
-			for (int i = 0; i < n - 1; i++) {
-				x1 = map[customer[i]][0];
-				y1 = map[customer[i]][1];
-				x2 = map[customer[i + 1]][0];
-				y2 = map[customer[i + 1]][1];
-				distance += Math.abs(x1 - x2) + Math.abs(y1 - y2);
+			for (int i = 1; i < N; i++) {
+				int[] next = customer[arr[i]];
+				distance += Math.abs(Math.abs(start[0] - next[0]) + Math.abs(start[1] - next[1]));
+				start = next;
+
+				if (i == N - 1) {
+					distance += Math.abs(Math.abs(next[0] - home[0]) + Math.abs(next[1] - home[1]));
+				}  // 마지막 고객집에서 집까지 복귀
 			}
 
-			// 마지막 고객 좌표에서 집까지 거리 계산
-			x1 = home[0];
-			y1 = home[1];
-			x2 = map[customer[n - 1]][0];
-			y2 = map[customer[n - 1]][1];
-			distance += Math.abs(x1 - x2) + Math.abs(y1 - y2);
-			
-			answer = Math.min(answer, distance);
+			answer = Math.min(distance, answer);
 			return;
 		}
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < N; i++) {
 			if (!visited[i]) {
 				visited[i] = true;
-				customer[depth] = i;
-				dfs(depth + 1);
+				arr[depth] = i;
+				permutation(depth + 1);
 				visited[i] = false;
 			}
 		}

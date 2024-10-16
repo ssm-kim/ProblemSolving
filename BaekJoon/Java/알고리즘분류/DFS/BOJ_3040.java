f24ecc149e3c11c3186ea50d72e77db7bc23841a
김성민 <rlacjs546@naver.com>
2024-10-17 00:34:05 +0900

    BOJ_3040_백설 공주와 일곱 난쟁이

--- BaekJoon/Java/알고리즘분류/DFS/BOJ_3040.java
+++ BaekJoon/Java/알고리즘분류/DFS/BOJ_3040.java
@@ -1,58 +1,40 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class main {
+public class Main {
 
-	static int[] arr, ans = new int[7];
+	static int N = 9;
+	static int[] map = new int[N];
+	static int[] idx = new int[7];
+	static boolean[] visited;
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("./input.txt"));
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		arr = new int[9];
-		for (int i = 0; i < 9; i++) {
-			arr[i] = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			map[i] = Integer.parseInt(br.readLine());
 		}
 
-		// 9개 중에 7가지를 뽑는 조합
-		// ans : static new int[7] 초기화 상태
-		combinations(0, 0, ans);
-		
-		// System.out.println(Arrays.toString(ans));
-		for (int x: ans) {
-			System.out.println(x);	
-		}
+		visited = new boolean[N];
+		dfs(0, 0, 0);
 	}
 
-	static boolean combinations(int depth, int start, int[] temp) {
-		
-		// 기저 조건 : 조합의 깊이(depth)가 7이 되면 현재 조합을 처리 
+	static void dfs(int depth, int start, int sum) {
 		if (depth == 7) {
-			int sum = 0;
-			for (int i : temp) {
-				sum += i;
-			}
-			// 수의 합이 100이 되었으면 정답 추출
 			if (sum == 100) {
-				return true;
+				for (int i : idx) {
+					System.out.println(map[i]);
+				}
 			}
-			return false;
+			return;
 		}
-		
-		for (int next_v = start; next_v < 9; next_v++) {
-			// 현재 깊이에서 temp 배열에 현재 인덱스 값을 저장
-			temp[depth] = arr[next_v];
-			
-			// 다음 깊이로 이동하고, 다음 값 추출을 위해 현재 인덱스 +1 
-			boolean state =combinations(depth + 1, next_v + 1, temp);
-			if (state)  // 결과를 찾았다면 즉시 반환
-				return true;
-			
-			// 인덱스를 사용했으면 다시 복구
-			temp[depth] = 0;
+
+		for (int i = start; i < N; i++) {
+			idx[depth] = i;
+			dfs(depth + 1, i + 1, sum + map[idx[depth]]);
 		}
-		return false;
 	}
 }

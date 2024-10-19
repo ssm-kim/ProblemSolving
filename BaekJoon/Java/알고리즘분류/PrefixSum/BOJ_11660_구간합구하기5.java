a49531bbd98088f5f74571d135754a41d8afbe0d
김성민 <rlacjs546@naver.com>
2024-10-20 01:37:39 +0900

    BOJ_11660_구간합구하기5

--- BaekJoon/Java/알고리즘분류/PrefixSum/BOJ_11660_구간합구하기5.java
+++ BaekJoon/Java/알고리즘분류/PrefixSum/BOJ_11660_구간합구하기5.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,14 +21,14 @@ public class Main {
 		map = new int[N + 1][N + 1];
 		prefix = new int[N + 1][N + 1];
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}  // 입력 받기
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			for (int j = 0; j < N; j++) {
 				prefix[i][j + 1] = prefix[i][j] + map[i][j + 1];
 			}
@@ -40,7 +41,7 @@ public class Main {
 			int x2 = Integer.parseInt(st.nextToken()), y2 = Integer.parseInt(st.nextToken());
 			int answer = 0;
 
-			for (int row = x1 - 1; row < x2; row++) {
+			for (int row = x1 ; row <= x2; row++) {
 				answer += prefix[row][y2] - prefix[row][y1 - 1];
 			}  // (x1 - 1) ~ x2 까지의 행만큼 반복 이후, 현재 행의 (y1 - 1) - y2 값을 빼서 구간 합 추출
 			sb.append(answer).append("\n");
@@ -50,7 +51,23 @@ public class Main {
 }
 
 
+
 /**  2차원 누적합
+ *  [0, 0, 0, 0, 0]
+ *  [0, 1, 3, 6, 10]
+ *  [0, 3, 8, 15, 24]
+ *  [0, 6, 15, 27, 42]
+ *  [0, 10, 24, 42, 64]
+ *  이 배열에서 각 숫자는 (1,1)부터 해당 위치까지의 모든 원소의 합을 나타냅니다.
+ *  이제 42, 10, 6, 1이 어떻게 나오는지 설명하겠습니다:
+ *
+ *  42: 이는 prefix[3][3]의 값입니다. 즉, (1,1)부터 (3,3)까지의 모든 원소의 합입니다.
+ *  10: 이는 prefix[1][3]의 값입니다. (1,1)부터 (1,3)까지의 합입니다.
+ *  6: 이는 prefix[3][1]의 값입니다. (1,1)부터 (3,1)까지의 합입니다.
+ *  1: 이는 prefix[1][1]의 값입니다. (1,1) 위치의 원래 값입니다.
+ *
+ *  이 값들이 중요한 이유는 (2,2)부터 (3,3)까지의 부분 합을 구하는 데 사용되기 때문입니다. 이 부분 합을 구하는 공식은 다음과 같습니다:
+ *
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -97,6 +114,6 @@ public class main {
 			sb.append(sum).append("\n");
 		}
 		System.out.println(sb);
-	}
+	 }
 }
  */

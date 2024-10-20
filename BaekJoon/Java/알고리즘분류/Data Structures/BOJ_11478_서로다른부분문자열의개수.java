9219a77274e6d00ba523738ca5304c1e2491b5b9
김성민 <rlacjs546@naver.com>
2024-10-21 00:06:20 +0900

    BOJ_11478_서로다른부분문자열의개수

--- BaekJoon/Java/알고리즘분류/Data Structures/BOJ_11478_서로다른부분문자열의개수.java
+++ BaekJoon/Java/알고리즘분류/Data Structures/BOJ_11478_서로다른부분문자열의개수.java
@@ -7,20 +7,19 @@ import java.util.Set;
 
 public class Main {
 
-    static int N;
     static Set<String> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        N = s.length();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j <= N; j++) {
-                set.add(s.substring(i, j));
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                set.add(s.substring(i, j + 1));
             }
         }
+
         System.out.println(set.size());
-   }
+    }
 }

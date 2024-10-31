85acabea6adf3a7aaeab02772d4b25f88b01f1f2
김성민 <rlacjs546@naver.com>
2024-11-01 01:38:42 +0900

    BOJ_10816_숫자카드2

---
+++ BaekJoon/Java/알고리즘분류/Data Structures/BOJ_10816_숫자카드2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static Map<Integer, Integer> map = new LinkedHashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            System.out.print(map.getOrDefault(num, 0) + " ");
+        }
+    }
+}

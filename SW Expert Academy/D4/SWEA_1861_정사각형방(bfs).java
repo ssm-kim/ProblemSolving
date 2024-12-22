8d18def8d1e7513e2f0ae5b76efa7a390cd213e1
김성민 <rlacjs546@naver.com>
2024-12-22 18:50:12 +0900

    SWEA_1861_정사각형방(bfs)

--- SW Expert Academy/D4/SWEA_1861_정사각형방(bfs).java
+++ SW Expert Academy/D4/SWEA_1861_정사각형방(bfs).java
@@ -1,82 +1,84 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-    static int n, maxMove, room;
+    static int N, roomMV, roomNum;
     static int[][] map;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
     static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = sc.nextInt();
-
+        int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
-            n = sc.nextInt();
-            map = new int[n][n];
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    map[i][j] = sc.nextInt();
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            maxMove = 0;
-            room = 0;
-            visited = new boolean[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    bfs(i, j);
-                    init();
+            visited = new boolean[N][N];
+            roomMV = 0;
+            roomNum = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int curMvRoom = bfs(i, j);
+
+                    if (curMvRoom > roomMV) {
+                        roomNum = map[i][j];
+                        roomMV = curMvRoom;
+                    }  // 최댓값 갱신 (방 번호 + 방 이동 갯수)
+                    else if (curMvRoom == roomMV) {
+                        roomNum = Math.min(roomNum, map[i][j]);
+                    }  // 둘 다 이동할 수 있는 방 갯수가 같다면
+
+                    for (boolean[] row : visited) {
+                        Arrays.fill(row, false);
+                    }  // 방문 초기화
                 }
             }
-            System.out.println("#" + tc + " " + room + " " + maxMove);
+            System.out.println("#" + tc + " " + roomNum + " " + roomMV);
         }
     }
 
-    static void init() {
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(visited[i], false);
-        }
-    }
-
-    static void bfs(int sx, int sy) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{sx, sy, 1, map[sx][sy]});  // 첫번째 방은 방문했으므로 1부터 시작
+    static int bfs(int sx, int sy) {
+        LinkedList<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sx, sy});
         visited[sx][sy] = true;
 
+        int mvCnt = 1;  // 현재 방 포함
         while (!queue.isEmpty()) {
             int[] pos = queue.poll();
             int cx = pos[0];
             int cy = pos[1];
-            int curMove = pos[2];   // 최대 몇개의 방을 이동할 수 있는지?
-            int curRoom = pos[3];   // 출발을 시작한 방 번호
-
-            if (curMove > maxMove) {
-                maxMove = curMove;
-                room = curRoom;
-            } else if (curMove == maxMove) {
-                room = Math.min(room, curRoom);
-            }
 
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) {
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) {
                     continue;
-                }
+                }  // 범위 검사 + 방문 검사
 
-                if (map[nx][ny] - map[cx][cy] == 1) {  // 정확히 1이 더 커야지 다음 방을 방문할 수 있다.
+                if (map[cx][cy] + 1 == map[nx][ny]) {
                     visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny, curMove + 1, curRoom});
-                }
+                    queue.offer(new int[]{nx, ny});
+                    mvCnt++;
+                }  // 이전 방과 다음 방이 1 차이가 나야 한다.
             }
         }
+        return mvCnt;
     }
 }

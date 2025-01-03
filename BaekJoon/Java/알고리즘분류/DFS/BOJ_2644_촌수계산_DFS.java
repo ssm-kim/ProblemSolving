464d3940dc5763dc5e0a6bda2f3a4de43f90f996
김성민 <rlacjs546@naver.com>
2025-01-04 00:45:42 +0900

    BOJ_2644_촌수계산

--- BaekJoon/Java/알고리즘분류/DFS/BOJ_2644_촌수계산_DFS.java
+++ BaekJoon/Java/알고리즘분류/DFS/BOJ_2644_촌수계산_DFS.java
@@ -1,59 +1,54 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class Main {
 
-	static int n, m, answer;
+	static int answer = -1;
+	static int N, M;
+	static int p1, p2;
 	static ArrayList<Integer>[] map;
 	static boolean[] visited;
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("src/input.txt"));
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		
-		n = sc.nextInt();       // 전체 사람 수
-		int p1 = sc.nextInt();  // 탐색할 부모
-		int p2 = sc.nextInt();  // 탐색할 자식
-		m = sc.nextInt();       // 부모 자식들 간의 관계
-		
-		map = new ArrayList[n + 1];  // 리스트 초기화
-		for (int i=1; i<=n; i++) {
-			map[i] = new ArrayList<>();
+		// 입력 받기
+		N = Integer.parseInt(br.readLine());    // 전체 사람 수
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		p1 = Integer.parseInt(st.nextToken());  // 촌수 계산 사람 1번
+		p2 = Integer.parseInt(st.nextToken());  // 촌수 계산 사람 2번
+		M = Integer.parseInt(br.readLine());    // 부모 자식들 간의 관계 갯수
+		map = new ArrayList[N + 1];             // 리스트 초기화
+		visited = new boolean[N + 1];           // 방문 배열 초기화
+		for (int i = 0; i <= N; i++) map[i] = new ArrayList<>();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int parent = Integer.parseInt(st.nextToken());
+			int child = Integer.parseInt(st.nextToken());
+			map[parent].add(child);  // 양방향
+			map[child].add(parent);
 		}
-		
-		for (int i=0; i<m; i++) {  // 양방향 연결
-			int v1 = sc.nextInt();
-			int v2 = sc.nextInt();
-			map[v1].add(v2);
-			map[v2].add(v1);
-		}
-		
-//		for (ArrayList<Integer> x : map) {
-//			System.out.println(x);
-//		}
-		
-		answer = -1;
-		visited = new boolean[n + 1];
-		dfs(p1, p2, 0);
+
+		dfs(p1, 0);
 		System.out.println(answer);
 	}
-	
-	static void dfs(int cur_v, int end, int cnt) {
-		if (cur_v == end) {
+
+	static void dfs(int cur_v, int cnt) {
+		if (cur_v == p2) {
 			answer = cnt;
 			return;
-		}
-		
+		}  // 나와 친척 관계가 있다.
+
 		visited[cur_v] = true;
 		for (int next_v : map[cur_v]) {
 			if (!visited[next_v]) {
-				dfs(next_v, end, cnt + 1);
+				dfs(next_v, cnt + 1);
 			}
 		}
 	}

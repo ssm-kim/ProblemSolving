ebb7cc066f7df2888167c0de9b6f7587f801f2f0
김성민 <rlacjs546@naver.com>
2025-01-04 16:45:00 +0900

    BOJ_13023_ABCDE

--- BaekJoon/Java/알고리즘분류/Backtracking/BOJ_13023_ABCDE.java
+++ BaekJoon/Java/알고리즘분류/Backtracking/BOJ_13023_ABCDE.java
@@ -1,57 +1,57 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-	static int n, m, answer;
+	static int N, M;
 	static ArrayList<Integer>[] map;
 	static boolean[] visited;
+	static boolean search;
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("src/input.txt"));
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		n = sc.nextInt();
-		m = sc.nextInt();
+		// 입력 받기
+		N = Integer.parseInt(st.nextToken());  // 사람의 수
+		M = Integer.parseInt(st.nextToken());  // 친구 관계의 수
+		map = new ArrayList[N];                // 0번부터 시작
+		for (int i = 0; i < N; i++) map[i] = new ArrayList<>();
 
-		map = new ArrayList[n]; // 0번부터 n-1까지 사용하며, 양방향 그래프
-		for (int i = 0; i < n; i++) {
-			map[i] = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			// 친구 관계이므로 양방향 !!
+			map[from].add(to);
+			map[to].add(from);
 		}
 
-		for (int i = 0; i < m; i++) {
-			int v1 = sc.nextInt();
-			int v2 = sc.nextInt();
-			map[v1].add(v2);
-			map[v2].add(v1);
-		}
+		visited = new boolean[N];
 
-		visited = new boolean[n];
-		answer = 0;
+		// 각 정점을 시작점으로 DFS 수행
+		for (int i = 0; i < N; i++) {
+			if (search) break;  // 이미 찾았다면 더 찾을 필요 없음
 
-		for (int i = 0; i < n; i++) {
-			if (answer != 1) {
-				dfs(i, 1);
-			}
+			dfs(0, i);
 		}
-		System.out.println(answer);
+		System.out.println(search ? 1 : 0);
 	}
 
-	static void dfs(int cur_v, int cnt) {
-		if (cnt == 5) { // 5명이 최대
-			answer = 1;
+	static void dfs(int depth, int cur_v) {
+		if (depth == 4) {
+			search = true;
 			return;
-		}
+		}  // 5명이 일렬로 연결되었다면 (depth가 4면 5번째 사람)
 
 		visited[cur_v] = true;
 		for (int next_v : map[cur_v]) {
 			if (!visited[next_v]) {
-				dfs(next_v, cnt + 1);
+				dfs(depth + 1, next_v);
 			}
 		}
 		visited[cur_v] = false;

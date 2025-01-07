3c8c770336873a110d8bfc10c09d779275be7409
김성민 <rlacjs546@naver.com>
2025-01-08 01:16:27 +0900

    BOJ_2468_안전영역

--- BaekJoon/02.Silver/BOJ_2468_안전영역.java
+++ BaekJoon/02.Silver/BOJ_2468_안전영역.java
@@ -68,3 +68,73 @@ public class Main {
     }
 }
 
+// dfs
+/*
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int answer = 0;
+    static int N, limitHeight;
+    static Set<Integer> heights = new HashSet<>();
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+
+        // 입력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                heights.add(map[i][j]);
+            }
+        }
+
+        for (Object height : heights.toArray()) {
+            limitHeight = (int) height;
+            visited = new boolean[N][N];
+            int cnt = 0;  // 현재 높이에서의 안전 영역 개수
+
+            // 모든 지점을 시작점으로 고려
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && map[i][j] > limitHeight) {
+                        cnt++;  // 새로운 안전 영역 발견
+                        dfs(i, j);
+                    }  // 방문하지 않았고, 현재 침수 높이보다 높은 지점에서 DFS 시작
+                }
+            }
+            answer = Math.max(answer, cnt);
+        }
+        System.out.println(answer == 0 ? 1 : answer);
+    }
+
+    static void dfs(int cx, int cy) {
+        visited[cx][cy] = true;  // 현재 위치 방문 처리
+
+        for (int i = 0; i < 4; i++) {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) continue;
+
+            if (map[nx][ny] > limitHeight) {
+                dfs(nx, ny);
+            }  // 다음 위치가 침수 높이보다 높으면 이동
+        }
+    }
+}
+ */

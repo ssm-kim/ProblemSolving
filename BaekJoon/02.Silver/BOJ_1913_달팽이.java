64c669829ae16d78204310af63e5c37f3fee95a5
김성민 <rlacjs546@naver.com>
2025-06-19 00:48:20 +0900

    BOJ_1913_달팽이

--- BaekJoon/02.Silver/BOJ_1913_달팽이.java
+++ BaekJoon/02.Silver/BOJ_1913_달팽이.java
@@ -1,62 +1,63 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int search = Integer.parseInt(br.readLine());
-        int[] answer = new int[2];
+        int n = Integer.parseInt(br.readLine());
+        int searchNum = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[N][N];
+        int[][] board = new int[n][n];
+
+        /**
+         * 9 2 3
+         * 8 1 4
+         * 7 6 5
+         */
         int cx = 0;
         int cy = 0;
         int dir = 0;
+        int[] answer = new int[2];
+        for (int i = n * n; i > 0; i--) {
+            board[cx][cy] = i;
 
-        // 바깥쪽에서 안쪽으로
-        for (int num = N * N; num > 0; num--) {
-            map[cx][cy] = num;
-            if (search == map[cx][cy]) {
+            if (board[cx][cy] == searchNum) {
                 answer[0] = cx + 1;
                 answer[1] = cy + 1;
-            }  // 탐색할 좌표값
+            }
 
             cx += dx[dir];
             cy += dy[dir];
 
-            if (0 <= cx && cx < N && 0 <= cy && cy < N && map[cx][cy] == 0) {
-                continue;
-            }
-            else {
-                // 복귀
+            // 범위를 벗어나거나 이미 0이 아니라면 방향을 바꿔준다.
+            if (cx < 0 || cx >= n || cy < 0 || cy >= n || board[cx][cy] != 0) {
+
+                // 복구
                 cx -= dx[dir];
                 cy -= dy[dir];
 
-                dir++;
-                if (dir == 4) {
-                    dir %= 4;
-                }
+                dir = (dir + 1) % 4;  // 꺽을 때 처리
 
-                // 방향 인덱스 증가 후 이동
+                // 방향 전환 이후 이동
                 cx += dx[dir];
                 cy += dy[dir];
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int[] row : map) {
-            for (int val : row) {
-                sb.append(val).append(" ");
+        for (int[] row : board) {
+            for (int col : row) {
+                sb.append(col).append(" ");
             }
             sb.append("\n");
         }
+
         sb.append(answer[0]).append(" ").append(answer[1]);
         System.out.println(sb);
     }

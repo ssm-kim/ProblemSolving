fe1d77dcb7b40a1a3a73a56aaebd7c359a8e9c06
김성민 <rlacjs546@naver.com>
2025-06-18 20:31:33 +0900

    BOJ_7562_나이트의이동

--- BaekJoon/02.Silver/BOJ_7562_나이트의이동.java
+++ BaekJoon/02.Silver/BOJ_7562_나이트의이동.java
@@ -1,69 +1,74 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
+
+class Point {
+    int x;
+    int y;
+    int moveCnt;
+
+    public Point(int x, int y, int moveCnt) {
+        this.x = x;
+        this.y = y;
+        this.moveCnt = moveCnt;
+    }
+}
 
 public class Main {
 
-    static int T, l;
-    static int[][] map;
-    static int[] dx = {1, 2, -1, -2, -1, -2, 1, 2};
-    static int[] dy = {2, 1, 2, 1, -2, -1, -2, -1};
+    static int l;
+    static boolean[][] visited;
+    static int[][] board;
+    static int[] dx = {-1, -2, 1, 2, -2, -1, 2, 1};
+    static int[] dy = {-2, -1, 2, 1, 1, 2, -1 ,-2};
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int tc = Integer.parseInt(br.readLine());
 
-        T = Integer.parseInt(br.readLine());
-
-        for (int tc = 0; tc < T; tc++) {
+        for (int t = 0; t < tc; t++) {
             l = Integer.parseInt(br.readLine());
+            board = new int[l][l];
 
-            map = new int[l][l];  // 체스판
             st = new StringTokenizer(br.readLine());
             int sx = Integer.parseInt(st.nextToken());
             int sy = Integer.parseInt(st.nextToken());
-            map[sx][sy] = 1;  // 출발지
 
             st = new StringTokenizer(br.readLine());
             int ex = Integer.parseInt(st.nextToken());
             int ey = Integer.parseInt(st.nextToken());
-            map[ex][ey] = -1;  // 목적지
 
-            int answer = bfs(sx, sy);
+            visited = new boolean[l][l];
+            int answer = bfs(sx, sy, ex, ey);
             System.out.println(answer);
         }
     }
 
-    static int bfs(int sx, int sy) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{sx, sy});
+    static int bfs(int sx, int sy, int ex, int ey) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(sx, sy, 0));
+        visited[sx][sy] = true;
 
         while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int cx = pos[0];
-            int cy = pos[1];
+            Point p = queue.poll();
+            int cx = p.x;
+            int cy = p.y;
+
+            if (cx == ex && cy == ey) {
+                return p.moveCnt;
+            }  // 목적지 도착
 
             for (int i = 0; i < 8; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if (nx < 0 || nx >= l || ny < 0 || ny >= l) {
-                    continue;
-                }  // 범위 검사
+                if (nx < 0 || nx >= l || ny < 0 || ny >= l || visited[nx][ny]) continue;
 
-                if (map[nx][ny] == -1) {
-                    return map[cx][cy];
-                }  // 목적지를 찾음
-
-                if (map[nx][ny] == 0) {
-                    map[nx][ny] = map[cx][cy] + 1;
-                    queue.offer(new int[] {nx, ny});
-                }
+                visited[nx][ny] = true;
+                queue.offer(new Point(nx, ny, p.moveCnt + 1));
             }
         }
-        return 0;
+        return -1;
     }
 }

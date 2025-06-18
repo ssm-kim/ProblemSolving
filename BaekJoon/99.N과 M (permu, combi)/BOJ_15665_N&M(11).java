ea65978ad681d65d6409b5f80653f0bd880d5f90
김성민 <rlacjs546@naver.com>
2025-06-18 17:53:23 +0900

    BOJ_15665_N&M(11)

---
+++ BaekJoon/99.N과 M (permu, combi)/BOJ_15665_N&M(11).java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] numbers, select;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 사전순 출력을 위해 입력 배열 정렬
+        Arrays.sort(numbers);
+
+        select = new int[m];
+        permutations(0);
+
+        String[] answer = set.toArray(new String[1]);
+
+        sb.setLength(0);
+        for (String s : answer) sb.append(s).append("\n");
+
+        System.out.println(sb);
+    }
+
+    static void permutations(int depth) {
+        // 종료 조건: m개 모두 선택했을 때
+        if (depth == m) {
+            for (int i : select) sb.append(i).append(" ");
+            set.add(sb.toString()); // 중복 수열 자동 제거
+            sb.setLength(0);
+            return;
+        }
+
+        // 백트래킹: start부터 끝까지 탐색
+        for (int i = 0; i < n; i++) {
+            select[depth] = numbers[i];
+            permutations(depth + 1); // 중복 순열
+        }
+    }
+}

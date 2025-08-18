5ce5e1dab2a3e9be917cd3971c0feb0652c5e597
김성민 <rlacjs546@naver.com>
2025-08-19 02:19:40 +0900

    BOJ_3190_뱀

--- BaekJoon/97.삼성SW역량테스트기출문제/BOJ_3190_뱀.java
+++ BaekJoon/97.삼성SW역량테스트기출문제/BOJ_3190_뱀.java
@@ -2,115 +2,115 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class snake {
-    int x, y;
-
-    public snake(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class snakeDirInfo {
+class transform {
     int seconds;
     char dir;
 
-    public snakeDirInfo(int seconds, char dir) {
+    public transform(int seconds, char dir) {
         this.seconds = seconds;
+        this.dir = dir;
+    }
+}
+
+class snake {
+    int x, y, dir;
+
+    public snake(int x, int y, int dir) {
+        this.x = x;
+        this.y = y;
         this.dir = dir;
     }
 }
 
 public class Main {
 
-    static int n, k, l;
+    static int n;
 
-    // 동 남 서 북
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    // 동 남 서 북 (시계 방향)
+    static int[] dx = new int[] {0, 1, 0, -1};
+    static int[] dy = new int[] {1, 0, -1, 0};
 
-    static boolean[][] visited;
     static int[][] map;
-    static Queue<snakeDirInfo> snakeDirInfo = new LinkedList<>();
+    static Queue<transform> info = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        k = Integer.parseInt(br.readLine());
         map = new int[n][n];
 
-        // 사과 위치
+        int k = Integer.parseInt(br.readLine());
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
-            int ax = Integer.parseInt(st.nextToken());
-            int ay = Integer.parseInt(st.nextToken());
-            map[ax-1][ay-1] = 2;  // 2는 사과 위치
+            // 인덱스 0 based
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            map[x][y] = 2;  // 사과의 위치
         }
 
-        // 뱀의 방향 변환 정보
-        l = Integer.parseInt(br.readLine());
+        int l = Integer.parseInt(br.readLine());
         for (int i = 0; i < l; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
+            int seconds = Integer.parseInt(st.nextToken());
             char dir = st.nextToken().charAt(0);
-            snakeDirInfo.offer(new snakeDirInfo(x, dir));
+            info.offer(new transform(seconds, dir));
         }
 
-        visited = new boolean[n][n];
-        System.out.println(simulate());
-    }
-
-    static int simulate() {
-        // 뱀 몸체 관리 덱 (앞: 머리, 뒤: 꼬리)
-        LinkedList<snake> queue = new LinkedList<>();
-        queue.offer(new snake(0, 0));
-        visited[0][0] = true;
-        int mySnakeDir = 0;  // 0:동, 1:남, 2:서, 3:북
-
-        int seconds = 0;  // 매초 갱신
+        LinkedList<snake> queue = new LinkedList<>();  // 뱀의 몸통 관리 (head부터 tail 순서)
+        queue.offer(new snake(0, 0, 0));
+        map[0][0] = 1;  // 뱀이 위치한 곳
+        int time = 0;
         while (true) {
-            seconds++;
+            time++;
 
-            // 다음 머리 위치 계산
-            snake snakeHead = queue.peek();
-            int nx = snakeHead.x + dx[mySnakeDir];
-            int ny = snakeHead.y + dy[mySnakeDir];
+            snake head = queue.peekLast();  // 현재 뱀 머리
 
-            // 충돌 검사 (벽 + 자기 몸)
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) break;
+            // 1. 뱀 머리를 현재 방향으로 한 칸 이동
+            int nx = head.x + dx[head.dir];
+            int ny = head.y + dy[head.dir];
 
-            // 사과 처리
+            // 2. 게임 종료 조건 체크
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) break;  // 벽 충돌
+            if (map[nx][ny] == 1) break;  // 자기 몸 충돌
+
+            // 3-1. 이동한 칸에 사과가 없다면
             if (map[nx][ny] == 2) {
-                map[nx][ny] = 0;  // 사과 제거, 꼬리 유지 (길이 증가)
+                map[nx][ny] = 1;  // 다음 위치의 사과 제거 후 머리 이동
             }
-            else {  // 꼬리 제거 및 방문 배열 회수 (길이 유지)
-                snake snakeTail = queue.pollLast();
-                visited[snakeTail.x][snakeTail.y] = false;
+            // 3-2. 이동한 칸에 사과가 없다면
+            else {
+                // 몸길이를 줄여서 꼬리가 위치한 칸을 비운다. 즉, 몸 길이는 변하지 않음.
+                snake tail = queue.poll();
+                map[tail.x][tail.y] = 0;
+
+                map[nx][ny] = 1;  // 다음 위치로 머리 이동
             }
 
-            // 새로운 머리 추가
-            visited[nx][ny] = true;
-            queue.offerFirst(new snake(nx, ny));
-
-            // 방향 전환 처리 (이동 후 체크)
-            snakeDirInfo front = snakeDirInfo.peek();
-            if (front != null && front.seconds == seconds) {
-                if (front.dir == 'L') {
-                    mySnakeDir = (mySnakeDir - 1 + 4) % 4;  // 왼쪽 방향 90도
+            // 4. 방향 전환 체크 및 새 머리 추가
+            if (!info.isEmpty()) {
+                transform front = info.peek();
+                if (time == front.seconds) {
+                    if (front.dir == 'D') {  // 오른쪽으로 90도 회전
+                        head.dir = (head.dir + 1) % 4;
+                    }
+                    else if (front.dir == 'L') {  // 왼쪽으로 90도 회전
+                        head.dir = (head.dir - 1 + 4) % 4;
+                    }
+                    info.poll();
                 }
-                else if (front.dir == 'D') {
-                    mySnakeDir = (mySnakeDir + 1) % 4;      // 오른쪽 방향 90도
-                }
-                snakeDirInfo.poll();
+                queue.offer(new snake(nx, ny, head.dir));
+            } else {
+                queue.offer(new snake(nx, ny, head.dir));
             }
+
         }
-        return seconds;
+
+        System.out.println(time);
     }
 }

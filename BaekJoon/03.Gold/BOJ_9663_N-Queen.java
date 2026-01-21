4824743396f7a3fbf1a14f29f1e0c3d00674fd5d
김성민 <rlacjs546@naver.com>
2026-01-21 14:57:04 +0900

    BOJ_9663_N-Queen

--- BaekJoon/03.Gold/BOJ_9663_N-Queen.java
+++ BaekJoon/03.Gold/BOJ_9663_N-Queen.java
@@ -1,78 +1,59 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
 
-public class Solution {
+public class Main {
 
-    static int n, answer;
-    static int[][] map;
+    static int n, answer = 0;
+    static int[][] board;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        System.setIn(new FileInputStream("./input.txt"));
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        board = new int[n][n];
 
-        int T = sc.nextInt();
-
-        for (int tc = 1; tc <= T; tc++) {
-            n = sc.nextInt();
-            map = new int[n][n];  // 입력 받기
-
-            // 문제 해결
-            answer = 0;  // 가능한 배치 수 초기화
-            dfs(0);  // 첫 번째 행부터 탐색 시작
-
-            // 출력
-            System.out.println("#" + tc + " " + answer);
-        }
+        dfs(0);
+        System.out.println(answer);
     }
 
     static void dfs(int row) {
         if (row == n) {
-            answer++;  // 퀸을 올바르게 배치한 경우
+            answer++;
             return;
         }
 
         for (int col = 0; col < n; col++) {
-            if (isSafe(row, col)) {
-                map[row][col] = 1;  // 퀸 배치
-                dfs(row + 1);  // 다음 행 이동
-                map[row][col] = 0;  // 백트래킹 (퀸 제거)
+            if (isPass(row, col)) {
+                board[row][col] = 1;
+                dfs(row + 1);
+                board[row][col] = 0;
             }
         }
     }
 
-    static boolean isSafe(int row, int col) {
-
-        // 같은 열에 퀸이 있는지 확인
-        for (int i = 0; i < row; i++) {
-            if (map[i][col] == 1) {
-                return false;
-            }
+    static boolean isPass(int sx, int sy) {
+        // 세로 위 확인
+        for (int nx = sx - 1; nx >= 0; nx--) {
+            if (board[nx][sy] == 1) return false;
         }
 
-        // 왼쪽 대각선에 퀸이 있는지 확인
-        int i = row - 1;
-        int j = col - 1;
-        while (i >= 0 && j >= 0) {
-            if (map[i][j] == 1) {
-                return false;
-            }
-            i--;
-            j--;
+        // 왼쪽 대각선 위 확인
+        int lt = sy;
+        for (int nx = sx - 1; nx >= 0; nx--) {
+            lt--;
+
+            if (lt < 0) break;
+            if (board[nx][lt] == 1) return false;
         }
 
-        // 오른쪽 대각선에 퀸이 있는지 확인
-        i = row - 1;
-        j = col + 1;
-        while (i >= 0 && j < n) {
-            if (map[i][j] == 1) {
-                return false;
-            }
-            i--;
-            j++;
+        // 오른쪽 대각선 위 확인
+        int rt  = sy;
+        for (int nx = sx - 1; nx >= 0; nx--) {
+            rt++;
+
+            if (rt >= n) break;
+            if (board[nx][rt] == 1) return false;
         }
 
         return true;
     }
 }
-
